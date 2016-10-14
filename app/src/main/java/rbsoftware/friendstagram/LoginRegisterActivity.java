ce6f7b8cccb1b849c01c1be436819b07acc71aa6
Rushil Perera <pererarushil1940@gmail.com>
2016-10-14 16:35:33 -0400

    Moved loading fragment to one method, and store registerFragment to retain state when switching back

--- app/src/main/java/rbsoftware/friendstagram/LoginRegisterActivity.java
+++ app/src/main/java/rbsoftware/friendstagram/LoginRegisterActivity.java
@@ -4,14 +4,17 @@ import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.content.Intent;
 import android.graphics.Color;
+import android.graphics.Typeface;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.TextView;
 
 import com.facebook.common.logging.FLog;
 import com.facebook.drawee.backends.pipeline.Fresco;
@@ -26,8 +29,8 @@ import java.util.Set;
 /**
  * A login screen that offers login via username/password.
  */
-public class LoginRegisterActivity extends AppCompatActivity implements LoginFragment.LoginListener, RegisterFragment.RegisterListener {
-
+public class LoginRegisterActivity extends AppCompatActivity implements LoginFragment.LoginListener, RegisterFragment
+        .RegisterListener, FragmentManager.OnBackStackChangedListener {
 
     /**
      * A dummy authentication store containing known user names and passwords.
@@ -42,6 +45,7 @@ public class LoginRegisterActivity extends AppCompatActivity implements LoginFra
     private UserLoginTask mAuthTask = null;
     private View mProgressView;
     private View mFormView;
+    private RegisterFragment registerFragment = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -70,7 +74,19 @@ public class LoginRegisterActivity extends AppCompatActivity implements LoginFra
         mFormView = findViewById(R.id.fragment_container);
         mProgressView = findViewById(R.id.login_progress);
 
+        TextView appName = (TextView) findViewById(R.id.app_name);
+        Typeface font = Typeface.createFromAsset(getAssets(), "billabong.ttf");
+        appName.setTypeface(font);
+
         loadLoginPage();
+    }
+
+    @Override
+    public void onBackStackChanged() {
+        FragmentManager manager = getSupportFragmentManager();
+        if (manager.getBackStackEntryCount() == 0) {
+            loadLoginBackground();
+        }
     }
 
     @Override
@@ -87,12 +103,9 @@ public class LoginRegisterActivity extends AppCompatActivity implements LoginFra
 
     @Override
     public void loadRegisterPage() {
-        FragmentManager manager = getSupportFragmentManager();
-        FragmentTransaction transaction = manager.beginTransaction();
-
-        RegisterFragment fragment = RegisterFragment.newInstance();
-        transaction.replace(R.id.fragment_container, fragment).addToBackStack("register");
-        transaction.commit();
+        RegisterFragment fragment = registerFragment == null ? new RegisterFragment() : registerFragment;
+        showFragment(fragment, true, true);
+        registerFragment = fragment;
 
         SimpleDraweeView background = (SimpleDraweeView) findViewById(R.id.background);
         String backgroundURI = "res:/" + R.drawable.register_bg;
@@ -102,24 +115,13 @@ public class LoginRegisterActivity extends AppCompatActivity implements LoginFra
     @Override
     public void loadLoginPage() {
         final FragmentManager manager = getSupportFragmentManager();
+        manager.addOnBackStackChangedListener(this);
         if (manager.getBackStackEntryCount() == 0) {
-            FragmentTransaction transaction = manager.beginTransaction();
-
-            LoginFragment fragment = LoginFragment.newInstance();
-            transaction.add(R.id.fragment_container, fragment);
-            transaction.commit();
+            LoginFragment fragment = new LoginFragment();
+            showFragment(fragment, false, false);
         } else {
             manager.popBackStack();
         }
-
-        manager.addOnBackStackChangedListener(new FragmentManager.OnBackStackChangedListener() {
-            @Override
-            public void onBackStackChanged() {
-                if (manager.getBackStackEntryCount() == 0) {
-                    loadLoginBackground();
-                }
-            }
-        });
 
         loadLoginBackground();
     }
@@ -128,6 +130,28 @@ public class LoginRegisterActivity extends AppCompatActivity implements LoginFra
         SimpleDraweeView background = (SimpleDraweeView) findViewById(R.id.background);
         String backgroundURI = "res:/" + R.drawable.login_bg;
         background.setImageURI(Uri.parse(backgroundURI));
+    }
+
+    private void showFragment(Fragment fragment, boolean replace, boolean addToStack) {
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+
+        if (addToStack) {
+            transaction.setCustomAnimations(R.anim.fade_in, R.anim.fade_out, R.anim.fade_out, R.anim.fade_in);
+        } else {
+            transaction.setCustomAnimations(R.anim.fade_in, R.anim.fade_out);
+        }
+
+        if (replace) {
+            transaction.replace(R.id.fragment_container, fragment);
+        } else {
+            transaction.add(R.id.fragment_container, fragment);
+        }
+
+        if (addToStack) {
+            transaction.addToBackStack(null);
+        }
+        transaction.commit();
     }
 
     /**
