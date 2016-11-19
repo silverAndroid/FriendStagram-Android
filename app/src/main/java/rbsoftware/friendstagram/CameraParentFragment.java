765a3185502a865895d130af4ca1848a59dde14a
Rushil Perera <pererarushil1940@gmail.com>
2016-11-19 08:26:20 -0500

    Added camera parent fragment to contain inner gallery and camera tabs

---
+++ app/src/main/java/rbsoftware/friendstagram/CameraParentFragment.java
@@ -0,0 +1,29 @@
+package rbsoftware.friendstagram;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class CameraParentFragment extends Fragment {
+
+
+    public CameraParentFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_camera_parent, container, false);
+    }
+
+}
