7aeb3e88399487a94e59f5e76db8f0014fd77363
Rushil Perera <pererarushil1940@gmail.com>
2016-11-20 23:33:20 -0500

    Switched to the implementation in the Design Library and readded PostsFragment

--- app/src/main/java/rbsoftware/friendstagram/MainActivity.java
+++ app/src/main/java/rbsoftware/friendstagram/MainActivity.java
@@ -1,15 +1,14 @@
 package rbsoftware.friendstagram;
 
 import android.os.Bundle;
-import android.support.annotation.IdRes;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-
-import com.roughike.bottombar.BottomBar;
-import com.roughike.bottombar.OnTabSelectListener;
+import android.view.MenuItem;
 
 import rbsoftware.friendstagram.model.Post;
 
@@ -21,19 +20,25 @@ public class MainActivity extends AppCompatActivity implements ProfileFragment.U
 
         setContentView(R.layout.activity_main);
 
-        BottomBar bottomBar = (BottomBar) findViewById(R.id.bottom_bar);
-        bottomBar.setOnTabSelectListener(new OnTabSelectListener() {
+        BottomNavigationView bottomNav = (BottomNavigationView) findViewById(R.id.bottom_nav);
+        bottomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
-            public void onTabSelected(@IdRes int tabId) {
-                if (tabId == R.id.tab_home) {
-                    showHomeFragment();
-                } else if (tabId == R.id.tab_account) {
-                    showAccountFragment();
-                } else if (tabId == R.id.tab_camera) {
-                    showPicturesFragment();
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.tab_home:
+                        showHomeFragment();
+                        return true;
+                    case R.id.tab_account:
+                        showAccountFragment();
+                        return true;
+                    case R.id.tab_camera:
+                        showPicturesFragment();
+                        return true;
                 }
+                return false;
             }
         });
+        showHomeFragment();
     }
 
     private void showHomeFragment() {
@@ -42,6 +47,10 @@ public class MainActivity extends AppCompatActivity implements ProfileFragment.U
 
     private void showAccountFragment() {
         showFragment(ProfileFragment.newInstance(this), false);
+    }
+
+    private void showPostFragment(Post post) {
+        showFragment(PostFragment.newInstance(post), true);
     }
 
     private void showPicturesFragment() {
@@ -60,7 +69,7 @@ public class MainActivity extends AppCompatActivity implements ProfileFragment.U
 
     @Override
     public void onImageClick(Post post) {
-        showPicturesFragment();
+        showPostFragment(post);
     }
 
     @Override
