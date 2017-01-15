e64476c1395595bafadd6e18a52d9eb018cee293
Rushil Perera <pererarushil1940@gmail.com>
2017-01-15 18:33:58 -0500

    Added API keys, Cloudinary library and removed Bottom Bar library

---
+++ app/src/main/java/rbsoftware/friendstagram/Constants.java
@@ -0,0 +1,13 @@
+package rbsoftware.friendstagram;
+
+/**
+ * Created by rushil.perera on 2017-01-15.
+ */
+
+public class Constants {
+    public class Cloudinary {
+        public static final String API_KEY = BuildConfig.CLOUDINARY_API_KEY;
+        public static final String API_SECRET = BuildConfig.CLOUDINARY_API_SECRET;
+        public static final String CLOUD_NAME = BuildConfig.CLOUDINARY_CLOUD_NAME;
+    }
+}
