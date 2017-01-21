7426bdb4dca1c593d7283f2781c9ce82e87f6d38
Rushil Perera <pererarushil1940@gmail.com>
2017-01-21 09:21:01 -0500

    Added Fragment to select filters (currently WIP)

---
+++ app/src/main/java/rbsoftware/friendstagram/SelectFilterFragment.java
@@ -0,0 +1,59 @@
+package rbsoftware.friendstagram;
+
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.facebook.drawee.view.SimpleDraweeView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link SelectFilterFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class SelectFilterFragment extends Fragment {
+    private static final String ARG_IMAGE_URI = "image_uri";
+
+    private Uri imageURI;
+
+    public SelectFilterFragment() {
+        // Required empty public constructor
+    }
+
+    public static SelectFilterFragment newInstance(Uri imageURI) {
+        SelectFilterFragment fragment = new SelectFilterFragment();
+        Bundle args = new Bundle();
+        args.putParcelable(ARG_IMAGE_URI, imageURI);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            imageURI = getArguments().getParcelable(ARG_IMAGE_URI);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_select_filter, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        SimpleDraweeView image = (SimpleDraweeView) view.findViewById(R.id.image);
+        image.setImageURI(imageURI);
+    }
+}
