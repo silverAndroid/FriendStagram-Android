8a5c1aaf12fc7f4bf5b5589f873b189454f35957
Rushil Perera <pererarushil1940@gmail.com>
2017-01-21 09:20:27 -0500

    Changed URL to type Uri

--- app/src/main/java/rbsoftware/friendstagram/model/Picture.java
+++ app/src/main/java/rbsoftware/friendstagram/model/Picture.java
@@ -1,23 +1,25 @@
 package rbsoftware.friendstagram.model;
 
+import android.net.Uri;
+
 /**
  * Created by silver_android on 1/3/2017.
  */
 
 public class Picture {
     private final int id;
-    private final String url;
+    private final Uri uri;
 
-    public Picture(int id, String url) {
+    public Picture(int id, Uri uri) {
         this.id = id;
-        this.url = url;
+        this.uri = uri;
     }
 
     public int getID() {
         return id;
     }
 
-    public String getURL() {
-        return url;
+    public Uri getURI() {
+        return uri;
     }
 }
