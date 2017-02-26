6ed64525f356de532a7e572d293f88de9df635ca
Rushil Perera <pererarushil1940@gmail.com>
2017-02-26 10:09:00 -0500

    Extends Serializable for Fragment arguments

--- app/src/main/java/rbsoftware/friendstagram/ToolbarManipulator.java
+++ app/src/main/java/rbsoftware/friendstagram/ToolbarManipulator.java
@@ -2,9 +2,11 @@ package rbsoftware.friendstagram;
 
 import android.support.v7.widget.Toolbar;
 
+import java.io.Serializable;
+
 /**
  * Created by silver_android on 24/10/16.
  */
-public interface ToolbarManipulator {
+public interface ToolbarManipulator extends Serializable {
     public void setToolbar(Toolbar toolbar);
 }
