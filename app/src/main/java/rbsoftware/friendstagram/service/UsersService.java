11bb0961d6bed7b936fe554bebf7c3df2ab6002f
Rushil Perera <pererarushil1940@gmail.com>
2017-02-26 10:10:06 -0500

    Modified endpoint to login and added endpoint to get user info

--- app/src/main/java/rbsoftware/friendstagram/service/UsersService.java
+++ app/src/main/java/rbsoftware/friendstagram/service/UsersService.java
@@ -6,7 +6,9 @@ import rbsoftware.friendstagram.model.Response;
 import rbsoftware.friendstagram.model.User;
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.GET;
 import retrofit2.http.POST;
+import retrofit2.http.Path;
 
 /**
  * Created by silver_android on 1/5/2017.
@@ -28,8 +30,11 @@ public class UsersService extends NetworkService {
         @POST("/users")
         Call<Response<User>> register(@Body Map<String, String> registerJSON);
 
-        @POST("/login")
+        @POST("/users/login")
         Call<Response<String>> login(@Body Map<String, String> loginJSON);
+
+        @GET("/users/{username}")
+        Call<Response<User>> getUser(@Path("username") String username);
 
         @POST("/logOff")
         Call<User> logout();
