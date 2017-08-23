0773c400811f479a337fe4cc079c88782b0a4e49
Rushil Perera <pererarushil1940@gmail.com>
2017-08-23 11:22:17 -0400

    Added AttributeSet to constructor

--- app/src/main/java/rbsoftware/friendstagram/workarounds/BillabongTextView.kt
+++ app/src/main/java/rbsoftware/friendstagram/workarounds/BillabongTextView.kt
@@ -3,12 +3,13 @@ package rbsoftware.friendstagram.workarounds
 import android.content.Context
 import android.graphics.Typeface
 import android.support.v7.widget.AppCompatTextView
+import android.util.AttributeSet
 
 /**
  * Created by silver_android on 25/10/16.
  */
 
-class BillabongTextView(context: Context) : AppCompatTextView(context) {
+class BillabongTextView(context: Context, attrs: AttributeSet) : AppCompatTextView(context, attrs) {
     init {
         if (!isInEditMode) {
             val font = Typeface.createFromAsset(context.assets, "billabong.ttf")
