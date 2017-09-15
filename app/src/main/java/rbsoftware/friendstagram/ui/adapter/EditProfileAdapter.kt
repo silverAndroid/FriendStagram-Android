dfed73c7ab2225758fb5d4b6784138fd003838e2
Rushil Perera <pererarushil1940@gmail.com>
2017-09-15 14:50:47 -0400

    Added update password and fixed input validations

--- app/src/main/java/rbsoftware/friendstagram/ui/adapter/EditProfileAdapter.kt
+++ app/src/main/java/rbsoftware/friendstagram/ui/adapter/EditProfileAdapter.kt
@@ -12,6 +12,8 @@ import android.widget.ImageView
 import rbsoftware.friendstagram.R
 import rbsoftware.friendstagram.Validators
 import rbsoftware.friendstagram.model.Validator
+import rbsoftware.friendstagram.validate
+import java.util.concurrent.atomic.AtomicInteger
 
 /**
  * Created by Rushil on 8/23/2017.
@@ -23,10 +25,10 @@ class EditProfileAdapter : RecyclerView.Adapter<EditProfileAdapter.EditProfileRo
         editableItems = listOf(
                 EditProfileItem("Name", R.drawable.ic_account, InputType.TYPE_TEXT_VARIATION_PERSON_NAME, listOf(Validator.empty())),
                 EditProfileItem("Username", inputType = InputType.TYPE_TEXT_FLAG_NO_SUGGESTIONS, validators = listOf(Validator.empty())),
-                EditProfileItem("Biography", R.drawable.ic_note_text, InputType.TYPE_TEXT_FLAG_MULTI_LINE, maxLines = 3),
-                EditProfileItem("Current Password", R.drawable.ic_lock, InputType.TYPE_TEXT_VARIATION_PASSWORD, listOf(Validator.empty(), Validator.passwordValid())),
-                EditProfileItem("New Password", inputType = InputType.TYPE_TEXT_VARIATION_PASSWORD, validators = listOf(Validator.empty(), Validator.passwordValid())),
-                EditProfileItem("Verify Password", inputType = InputType.TYPE_TEXT_VARIATION_PASSWORD, validators = listOf(Validator.empty(), Validator.passwordValid()))
+                EditProfileItem("Biography", R.drawable.ic_note_text, InputType.TYPE_TEXT_FLAG_MULTI_LINE, maxLines = 3, key = "bio"),
+                EditProfileItem("Current Password", R.drawable.ic_lock, InputType.TYPE_TEXT_VARIATION_PASSWORD, listOf(Validator.empty(), Validator.passwordValid()), key = "password.old"),
+                EditProfileItem("New Password", inputType = InputType.TYPE_TEXT_VARIATION_PASSWORD, validators = listOf(Validator.empty(), Validator.passwordValid()), key = "password.new"),
+                EditProfileItem("Verify Password", inputType = InputType.TYPE_TEXT_VARIATION_PASSWORD, validators = listOf(Validator.empty(), Validator.passwordValid()), key = null)
         )
     }
 
@@ -38,21 +40,70 @@ class EditProfileAdapter : RecyclerView.Adapter<EditProfileAdapter.EditProfileRo
     override fun onBindViewHolder(holder: EditProfileRow?, position: Int) {
         val item: EditProfileItem = editableItems[position]
 
-        with (item) {
+        with(item) {
             holder?.hint?.hint = hint
             holder?.input?.inputType = inputType
             holder?.input?.maxLines = maxLines
+            holder?.hint?.id = uniqueID
         }
 
         item.icon?.let { holder?.icon?.setImageResource(it) }
-        holder?.input?.let { input ->
-            item.validators.forEach {
-                Validators.addValidation(input.id, it)
-            }
+        holder?.hint?.let { editableItems[position].input = it }
+        item.validators.forEach {
+            Validators.addValidation(item.uniqueID, it)
         }
     }
 
     override fun getItemCount(): Int = editableItems.size
+
+    fun getData(): Map<String, Any>? {
+        var isValid = true
+        editableItems.forEach { editProfileItem ->
+            editProfileItem.validators.forEach {
+                isValid = editProfileItem.input.validate() && isValid
+            }
+        }
+        return if (isValid) {
+            createNestedMap(editableItems.map { it.getMap() })
+        } else {
+            null
+        }
+    }
+
+    private fun createNestedMap(mapList: List<Map<String, Any>>): MutableMap<String, Any> {
+        val map: MutableMap<String, Any> = mutableMapOf()
+        mapList.forEach { mapObj ->
+            mapObj.keys.forEach { key ->
+                if (mapObj[key] is String) {
+                    map[key] = mapObj[key] as String
+                } else {
+                    map[key] = addToMap(mapObj.toMutableMap(), map, key)
+                }
+            }
+        }
+        return map
+    }
+
+    private fun createNestedMap(mutableMap: MutableMap<String, Any>): MutableMap<String, Any> {
+        val map: MutableMap<String, Any> = mutableMapOf()
+        mutableMap.keys.forEach { key ->
+            if (mutableMap[key] is String) {
+                map[key] = mutableMap[key] as String
+            } else {
+                map[key] = addToMap(mutableMap, map, key)
+            }
+        }
+        return map
+    }
+
+    private fun addToMap(map: MutableMap<String, Any>, returnMap: MutableMap<String, Any>, key: String): MutableMap<String, Any> {
+        val nestedMap = createNestedMap(map[key] as MutableMap<String, Any>)
+        return if (returnMap[key] == null) {
+            nestedMap
+        } else {
+            (returnMap[key] as MutableMap<String, Any>).plus(nestedMap).toMutableMap()
+        }
+    }
 
     inner class EditProfileRow(itemView: View?) : RecyclerView.ViewHolder(itemView) {
         val icon: ImageView? = itemView?.findViewById(R.id.icon)
@@ -60,5 +111,41 @@ class EditProfileAdapter : RecyclerView.Adapter<EditProfileAdapter.EditProfileRo
         val hint: TextInputLayout? = itemView?.findViewById(R.id.input_hint)
     }
 
-    data class EditProfileItem(val hint: String, @DrawableRes val icon: Int? = null, val inputType: Int = InputType.TYPE_CLASS_TEXT, val validators: List<Validator> = listOf(), val maxLines: Int = 1)
+    data class EditProfileItem(val hint: String, @DrawableRes val icon: Int? = null, val inputType: Int = InputType.TYPE_CLASS_TEXT, val validators: List<Validator> = listOf(), private val key: String? = hint.toLowerCase(), val maxLines: Int = 1) {
+        val uniqueID: Int = generateViewId()
+        lateinit var input: TextInputLayout
+
+        fun getMap(): MutableMap<String, Any> {
+            var map = mutableMapOf<String, Any>()
+            key?.let {
+                it.split(".")
+                        .asReversed()
+                        .forEach {
+                            map = if (map.isEmpty()) {
+                                mutableMapOf(it to input.editText?.text.toString())
+                            } else {
+                                mutableMapOf(it to map)
+                            }
+                        }
+            }
+            return map
+        }
+
+        private fun generateViewId(): Int {
+            while (true) {
+                val result = sNextGeneratedId.get()
+                // aapt-generated IDs have the high byte nonzero; clamp to the range under that.
+                var newValue = result + 1
+                if (newValue > 0x00FFFFFF) newValue = 1 // Roll over to 1, not 0.
+                if (sNextGeneratedId.compareAndSet(result, newValue)) {
+                    return result
+                }
+            }
+        }
+
+        companion object {
+            @JvmStatic
+            private val sNextGeneratedId = AtomicInteger(1)
+        }
+    }
 }
