a4e7a90164f838a6f372c7571e5f71b0f9db852a
Rushil Perera <pererarushil1940@gmail.com>
2017-11-26 17:06:49 -0500

    Added function to prefetch image

--- app/src/main/java/rbsoftware/friendstagram/service/ImageService.kt
+++ app/src/main/java/rbsoftware/friendstagram/service/ImageService.kt
@@ -1,10 +1,21 @@
 package rbsoftware.friendstagram.service
 
+import android.os.Looper
+import android.util.Log
 import com.cloudinary.Cloudinary
+import com.facebook.datasource.DataSource
+import com.facebook.datasource.DataSubscriber
+import com.facebook.drawee.backends.pipeline.Fresco
+import com.facebook.imagepipeline.request.ImageRequest
+import io.reactivex.Completable
 import io.reactivex.Single
 import rbsoftware.friendstagram.Constants
 import java.io.IOException
 import java.io.InputStream
+import java.util.concurrent.ArrayBlockingQueue
+import java.util.concurrent.Executor
+import java.util.concurrent.ThreadPoolExecutor
+import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 
 /**
@@ -42,5 +53,49 @@ class ImageService @Inject constructor() {
 
     companion object {
         private val TAG = "ImageService"
+        private val handler = Looper.getMainLooper()
+
+        fun prefetchImage(imageRequest: ImageRequest, prefetchToDisk: Boolean = true): Completable {
+            return Completable.create {
+                val dataSource = if (prefetchToDisk) {
+                    Fresco.getImagePipeline().prefetchToDiskCache(
+                            imageRequest,
+                            null
+                    )
+                } else {
+                    Fresco.getImagePipeline().prefetchToBitmapCache(
+                            imageRequest,
+                            null
+                    )
+                }
+                if (!dataSource.isFinished) {
+                    dataSource.subscribe(object : DataSubscriber<Void> {
+                        override fun onNewResult(dataSource: DataSource<Void>) {
+                            it.onComplete()
+                        }
+
+                        override fun onCancellation(dataSource: DataSource<Void>) {
+                            it.onComplete()
+                        }
+
+                        override fun onProgressUpdate(dataSource: DataSource<Void>) {
+                            Log.v(TAG, "progress: ${dataSource.progress}")
+                        }
+
+                        override fun onFailure(dataSource: DataSource<Void>) {
+                            it.onError(Throwable(dataSource.failureCause))
+                        }
+                    }, ThreadPoolExecutor(
+                            1,
+                            Runtime.getRuntime().availableProcessors(),
+                            120,
+                            TimeUnit.SECONDS,
+                            ArrayBlockingQueue<Runnable>(1)
+                    ))
+                } else {
+                    it.onComplete()
+                }
+            }
+        }
     }
 }
